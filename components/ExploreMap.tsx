7ff37b7d8f3bcd3381e49099e5145778b75ebf81
tfs2006 <fastwealth@4ourmedia.com>
2026-09-25 16:56:57 -0600

    fix-explore-map-tiles-no-api-key

--- components/ExploreMap.tsx
+++ components/ExploreMap.tsx
@@ -366,8 +366,8 @@ export const ExploreMap: React.FC<ExploreMapProps> = ({ isLoggedIn }) => {
               zoomControl
             >
               <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-                url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
               />
               <FitUS />
               <MapResizer trigger={!!selected} />
